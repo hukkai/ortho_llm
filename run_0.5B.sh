d0c597d7224a8a0bc38ba80917e53a0257631612
Kai Hu <kaiorhu@gmail.com>
2026-04-01 14:55:10 -0400

    Add so-lr parameter to run_0.5B.sh

--- run_0.5B.sh
+++ run_0.5B.sh
@@ -6,6 +6,7 @@ SUB_MATRIX=${2:-1}
 
 LR=${3:-2e-4}
 MIN_LR=${4:-2e-5}
+SO_LR=${5:-0.5}
 
 OMP_NUM_THREADS=1 torchrun \
     --nproc_per_node 8 \
@@ -20,6 +21,7 @@ OMP_NUM_THREADS=1 torchrun \
     --seq-length 2048 \
     --lr $LR \
     --min-lr $MIN_LR \
+    --so-lr $SO_LR \
     --num-steps 10_000 \
     --orthogonal-type "${ORTH_TYPE}" \
     --sub-matrix "${SUB_MATRIX}"
